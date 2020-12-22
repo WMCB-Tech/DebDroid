6e27eebeb8ce77f28d1b733971f92410c388f1b5
WMCB-Tech <bautista.wyatt.marcus30@gmail.com>
2020-12-22 15:32:35 +0800

    Add Dialog-Based Configurations and Set Environment Variables
    Set MOZ_FAKE_NO_SANDBOX=1 by default #18
    
    Add Dialog-based Prompt #15 (partial)

--- prefix-conf/config/distro-config.sh
+++ prefix-conf/config/distro-config.sh
@@ -18,31 +18,35 @@ rm -rf /etc/apt/apt.conf.d/docker-* >/dev/null 2>&1 || true
 apt update
 apt upgrade -y || true
 apt install -f -y || true
-apt install nano sudo busybox udisks2 dbus-x11 locales pulseaudio procps --no-install-recommends -y || true
+apt install nano sudo busybox udisks2 dbus-x11 locales pulseaudio procps tzdata dialog --no-install-recommends --no-install-suggests -y || true
 apt install -f -y || true
 dpkg --configure -a || true
 apt autoremove -y || true
 apt clean || true
 
-## Fix Problems
+## Setup Environment Variables
 echo ""
-echo "Setting up Environment Variables"
+echo "Setting up Environment Variables..."
 echo "Adding /sbin path for non-root users"
 echo "export PATH=/usr/local/bin:/usr/local/sbin:/usr/local/games:/usr/bin:/usr/sbin:/usr/games:/bin:/sbin" > /etc/profile.d/debdroid-path.sh
 echo "export LANG=C.UTF-8" > /etc/profile.d/debdroid-lang.sh
 echo "export PULSE_SERVER=127.0.0.1" > /etc/profile.d/debdroid-pulseserver.sh
+echo "export MOZ_FAKE_NO_SANDBOX=1" > /etc/profile.d/debdroid-firefox-fix.sh
+echo "$(cat /.proot.distinfo)" > /etc/debian_chroot
 
-## Configure udisks2 and dbus as if udisks2 interrupts apt
+## Configure Packages
 echo ""
-echo "Configuring Udisks2.... "
+echo "Configuring Packages... "
 echo "" > /var/lib/dpkg/info/udisks2.postinst
 echo "" > /var/lib/dpkg/info/dbus.postinst
 dpkg --configure -a
+dpkg-reconfigure tzdata || true
 
 ## Setup User Accounts
 echo "Configuring User Accounts.... "
-read -p "Enter your UNIX username (lower case): " username
+username=$(dialog --title "Finish Debian Setup" --nocancel --inputbox "Enter your desired username for your default user account" 9 40 3>&1 1>&2 2>&3 3>&- )
 useradd -s /bin/bash -m $username
+echo ""
 passwd $username
 echo "Adding the user to sudoers for sudo access"
 echo "$username ALL=(ALL:ALL) NOPASSWD:ALL" > /etc/sudoers.d/$username
@@ -56,3 +60,4 @@ which busybox >/dev/null 2>&1
 which pulseaudio >/dev/null 2>&1
 which dbus-launch >/dev/null 2>&1
 which nano >/dev/null 2>&1
+which dialog >/dev/null 2>&1
